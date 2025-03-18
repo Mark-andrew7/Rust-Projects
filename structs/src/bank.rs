ec0516a0fceebf4783812acecbe27ddbbe536ea0
Mark-andrew7 <kamaumark19@gmail.com>
2025-03-18 09:46:13 +0000

    Simple banking program using structs

--- structs/src/bank.rs
+++ structs/src/bank.rs
@@ -19,6 +19,30 @@ impl BankAccount {
     }
 
     fn display_balance(&self) {
-        println!("Current balance is {}", self.balance)
+        println!("Current balance for {} is {}", self.owner, self.balance)
     }
+}
+
+fn main() {
+    let mut account = BankAccount {
+        owner: String::from("Mark"),
+        balance: 100.0,
+    };
+
+    account.display_balance();
+
+    account.deposit(50.0);
+    println!("After depositing 50:");
+    account.display_balance();
+
+    let withdrawal_success = account.withdraw(30.0);
+    println!("Withdrawal of 30 successful: {}", withdrawal_success);
+    account.display_balance();
+
+
+    let withdrawal_success = account.withdraw(200.0);
+    println!("Withdrawal of 200 successful: {}", withdrawal_success);
+    account.display_balance();
+
+
 }
